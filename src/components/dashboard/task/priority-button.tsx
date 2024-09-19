0ec73ef05bc9e0e0480bdefaf1327caa36a5d7e8
Kahui <kahuiw123@gmail.com>
2024-09-19 10:42:36 -0400

    refactored priority button

--- src/components/dashboard/task/priority-button.tsx
+++ src/components/dashboard/task/priority-button.tsx
@@ -8,13 +8,19 @@ const priorityColors = [
   { border: "border-gray-300", text: "text-gray-300" },
 ];
 
-export const PriorityButton = ({ priority }: { priority: number }) => {
+interface PriorityButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  priority: number;
+}
+
+export const PriorityButton = ({ priority, ...props }: PriorityButtonProps) => {
   const { border, text } = priorityColors[priority - 1];
   return (
     <Button
       variant="ghost"
       size="icon"
       className={`group mt-0.5 size-fit rounded-full border-2 ${border} bg-transparent`}
+      {...props}
     >
       <FaCheck
         className={`size-4 rounded-full border border-transparent ${text} opacity-0 transition-opacity duration-200 hover:opacity-100`}
--- src/components/dashboard/task/task-card.tsx
+++ src/components/dashboard/task/task-card.tsx
@@ -6,11 +6,15 @@ import { Button } from "../../ui/button";
 import { TaskCardDropDown } from "./task-card-dropdown";
 import { Task } from "@prisma/client";
 import { PriorityButton } from "./priority-button";
+import { removeTask } from "@/data/actions";
 
 const TaskCard = ({ task }: { task: Task }) => {
   return (
     <div className="transtion-all group flex w-full max-w-full items-start gap-2 rounded-lg border border-transparent p-2 shadow-sm duration-300 hover:border-gray-300 hover:shadow-md">
-      <PriorityButton priority={task.priority} />
+      <PriorityButton
+        priority={task.priority}
+        onClick={() => removeTask(task.id)}
+      />
       <div className="flex flex-1 flex-col justify-center overflow-hidden">
         <div className="flex items-center justify-between">
           <h3>{task.title}</h3>
