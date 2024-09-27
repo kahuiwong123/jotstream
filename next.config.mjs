b3f892f19e96392ecd3def01198df0ee4d29755e
Kahui <kahuiw123@gmail.com>
2024-09-27 11:10:55 -0400

    added prisma monorepo workaround

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 const nextConfig = {
   transpilePackages: ["@prisma/client"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
