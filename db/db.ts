f2bbb7edaf874bba2049319cdbe5ba097104f621
kahuiwong123 <kahuiw123@gmail.com>
2026-08-18 11:21:45 -0400

    updated prisma client

--- db/db.ts
+++ db/db.ts
@@ -1,15 +1,16 @@
-import { PrismaClient } from "../prisma/generated/client";
+import { PrismaClient } from "../prisma/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
 };
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from "../prisma/generated/client";
-
-const prisma = new PrismaClient();
+import prisma from "./db";
 
 async function main() {
   const tasks = await prisma.section.findMany({
