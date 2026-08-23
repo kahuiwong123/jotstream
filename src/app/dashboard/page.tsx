4c08da97091eeb06e093cdda253a592ab77a7d10
kahuiwong123 <kahuiw123@gmail.com>
2026-08-23 18:02:58 -0400

    refactored drag and drop frontend functionality with dnd-kit@latest

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,159 +3,82 @@
 
 import AddSectionButton from "@/components/dashboard/section/add-section-button";
 import SectionCard from "@/components/dashboard/section/section-card";
-import TaskCard from "@/components/dashboard/task/task-card";
 import { moveSection, moveTask } from "@/data/actions";
 import { useSectionStore } from "@/data/store/sectionStore";
 import { useTaskStore } from "@/data/store/taskStore";
-import { sectionProps } from "@/data/types";
+import { Task, Section } from "~/generated/prisma/client";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { move } from "@dnd-kit/helpers";
+import { KeyboardSensor, PointerSensor } from "@dnd-kit/dom";
 import {
-  DndContext,
-  DragEndEvent,
-  DragOverEvent,
+  DragDropEventHandlers,
+  DragDropProvider,
   DragOverlay,
-  DragStartEvent,
-  PointerSensor,
-  TouchSensor,
-  useSensor,
-  useSensors,
-} from "@dnd-kit/core";
-import { arrayMove, SortableContext } from "@dnd-kit/sortable";
-import { Task } from "~/generated/prisma/client";
-import { useEffect, useState } from "react";
-import { createPortal } from "react-dom";
+} from "@dnd-kit/react";
 
 export default function Page() {
   const tasksData = useTaskStore((state) => state.tasks);
   const sections = useSectionStore((state) => state.sections);
-  const setSections = useSectionStore((state) => state.setSections);
 
-  const [tasks, setTasks] = useState<Task[]>(tasksData);
+  const [items, setItems] = useState<Record<string, Task[]>>({});
 
-  const [activeSection, setActiveSection] = useState<
-    sectionProps | undefined
-  >();
-  const [activeTask, setActiveTask] = useState<Task | undefined>();
+  const sensors = [
+    PointerSensor.configure({
+      activatorElements(source) {
+        return [source.element, source.handle];
+      },
+    }),
+    KeyboardSensor,
+  ];
 
   useEffect(() => {
-    setTasks(tasksData);
-  }, [tasksData]);
-
-  const handleDragEnd = async ({ active, over }: DragEndEvent) => {
-    setActiveSection(undefined);
-    setActiveTask(undefined);
-
-    if (!over) {
-      return;
-    }
-
-    const activeType = active.data.current?.type;
-    const overType = over.data.current?.type;
-
-    if (activeType === "section" && overType === "section") {
-      const oldIndex = sections.findIndex(
-        (section) => section.id === active.id,
-      );
-      const newIndex = sections.findIndex((section) => section.id === over.id);
-      if (oldIndex !== -1 && newIndex !== -1) {
-        setSections(arrayMove(sections, oldIndex, newIndex));
-      }
-      await moveSection(active.id.toString(), over.id.toString());
-    }
-  };
-
-  const handleDragStart = ({ active }: DragStartEvent) => {
-    if (active.data.current?.type === "section") {
-      setActiveSection(active.data.current?.section);
-      return;
-    }
-
-    if (active.data.current?.type === "task") {
-      setActiveTask(active.data.current?.task);
-      return;
-    }
-  };
-
-  const handleDragOver = async ({ active, over }: DragOverEvent) => {
-    if (!over || active.id === over.id) {
-      return;
-    }
-
-    const activeType = active.data.current?.type;
-    const overType = over.data.current?.type;
-
-    if (activeType !== "task") {
-      return;
-    }
-
-    if (overType === "task") {
-      setTasks((tasks) => {
-        const activeIndex = tasks.findIndex((task) => task.id === active.id);
-        const overIndex = tasks.findIndex((task) => task.id === over.id);
-        tasks[activeIndex].sectionId = tasks[overIndex].sectionId;
-        return arrayMove(tasks, activeIndex, overIndex);
-      });
-    } else {
-      setTasks((tasks) => {
-        const activeIndex = tasks.findIndex((task) => task.id === active.id);
-        tasks[activeIndex].sectionId = over.id.toString();
-        return arrayMove(tasks, activeIndex, activeIndex);
-      });
-    }
-
-    await moveTask(
-      active.id.toString(),
-      over.id.toString(),
-      overType === "section",
+    setItems(
+      Object.fromEntries(
+        sections.map((section) => [
+          section.id,
+          tasksData.filter((task) => task.sectionId === section.id),
+        ]),
+      ),
     );
-  };
+  }, [tasksData, sections]);
 
-  const sensors = useSensors(
-    useSensor(PointerSensor, {
-      activationConstraint: {
-        distance: 10,
-      },
-    }),
-    useSensor(TouchSensor, {
-      activationConstraint: {
-        distance: 10,
-      },
-    }),
-  );
+  const snapshot = useRef(structuredClone(items));
 
   return (
     <div className="flex h-full grow gap-8 bg-white-main dark:bg-dark-main">
       <div className="flex grow gap-8">
-        <DndContext
+        <DragDropProvider
+          onDragStart={useCallback<DragDropEventHandlers["onDragStart"]>(() => {
+            snapshot.current = structuredClone(items);
+          }, [items])}
+          onDragOver={useCallback<DragDropEventHandlers["onDragOver"]>(
+            (event) => {
+              const { source } = event.operation;
+              if (source && source.type === "section") return;
+              setItems((items) => move(items, event));
+            },
+            [],
+          )}
+          onDragEnd={useCallback<DragDropEventHandlers["onDragEnd"]>(
+            (event) => {
+              if (event.canceled) {
+                setItems(snapshot.current);
+                return;
+              }
+            },
+            [],
+          )}
           sensors={sensors}
-          onDragEnd={handleDragEnd}
-          onDragStart={handleDragStart}
-          onDragOver={handleDragOver}
         >
-          <SortableContext items={sections.map((section) => section.id)}>
-            {sections.map((section) => (
-              <SectionCard
-                key={section.id}
-                section={section}
-                tasks={tasks.filter((task) => task.sectionId === section.id)}
-              />
-            ))}
-          </SortableContext>
-          {typeof window === "object" &&
-            createPortal(
-              <DragOverlay>
-                {activeSection && (
-                  <SectionCard
-                    section={activeSection}
-                    tasks={tasks.filter(
-                      (task) => task.sectionId === activeSection.id,
-                    )}
-                  />
-                )}
-                {activeTask && <TaskCard task={activeTask} />}
-              </DragOverlay>,
-              document.body,
-            )}
-        </DndContext>
+          {sections.map((section, index) => (
+            <SectionCard
+              key={section.id}
+              index={index}
+              section={section}
+              tasks={items[section.id] ?? []}
+            />
+          ))}
+        </DragDropProvider>
         <AddSectionButton />
       </div>
     </div>
